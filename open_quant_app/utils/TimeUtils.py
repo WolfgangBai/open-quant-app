216da2e86aa8b5d9892801a4c11e3aec9f1a5c92
openeh <625529334@qq.com>
2023-09-09 19:27:07 +0800

    2023-9-9-refactor: time utils

--- open_quant_app/utils/TimeUtils.py
+++ open_quant_app/utils/TimeUtils.py
@@ -1,58 +1,47 @@
-import datetime
+from datetime import datetime, timedelta
 
 
 class TimeUtils:
     @staticmethod
-    def get_morning_start() -> datetime.datetime:
-        return datetime.datetime.now().replace(hour=9, minute=30, second=0)
+    def get_morning_start(timestamp: datetime = datetime.now()) -> datetime:
+        return timestamp.replace(hour=9, minute=30, second=0)
 
     @staticmethod
-    def get_morning_end() -> datetime.datetime:
-        return datetime.datetime.now().replace(hour=11, minute=30, second=0, microsecond=0)
+    def get_morning_end(timestamp: datetime = datetime.now()) -> datetime:
+        return timestamp.replace(hour=11, minute=30, second=0, microsecond=0)
 
     @staticmethod
-    def get_afternoon_start() -> datetime.datetime:
-        return datetime.datetime.now().replace(hour=13, minute=0, second=0)
+    def get_afternoon_start(timestamp: datetime = datetime.now()) -> datetime:
+        return timestamp.replace(hour=13, minute=0, second=0)
 
     @staticmethod
-    def get_afternoon_end() -> datetime.datetime:
-        return datetime.datetime.now().replace(hour=15, minute=0, second=0)
+    def get_afternoon_end(timestamp: datetime = datetime.now()) -> datetime:
+        return timestamp.replace(hour=15, minute=0, second=0)
 
     @staticmethod
-    def get_trade_time_seg() -> [datetime.datetime]:
-        morning_start = datetime.datetime.now().replace(hour=9, minute=30, second=0, microsecond=0)
-        morning_end = morning_start + datetime.timedelta(hours=2)
+    def get_trade_time_seg() -> [datetime]:
+        morning_start = datetime.now().replace(hour=9, minute=30, second=0, microsecond=0)
+        morning_end = morning_start + timedelta(hours=2)
 
-        afternoon_start = datetime.datetime.now().replace(hour=13, minute=0, second=0, microsecond=0)
-        afternoon_end = afternoon_start + datetime.timedelta(hours=2)
+        afternoon_start = datetime.now().replace(hour=13, minute=0, second=0, microsecond=0)
+        afternoon_end = afternoon_start + timedelta(hours=2)
 
         trade_times = [morning_start, morning_end, afternoon_start, afternoon_end]
         return trade_times
 
     @staticmethod
-    def judge_trade_time(timestamp: datetime.datetime) -> bool:
-        now = datetime.datetime.now()
-        year, month, day = now.year, now.month, now.day
-        morning_start = datetime.datetime(year, month, day, 9, 30, 0)
-        morning_end = datetime.datetime(year, month, day, 11, 30, 0)
-        afternoon_start = datetime.datetime(year, month, day, 13, 0, 0)
-        afternoon_end = datetime.datetime(year, month, day, 15, 0, 0)
-        return (morning_start <= timestamp <= morning_end) or (
-                afternoon_start <= timestamp <= afternoon_end)
+    def judge_trade_time(timestamp: datetime) -> bool:
+        return (TimeUtils.get_morning_start(timestamp) <= timestamp <= TimeUtils.get_morning_end(timestamp)) or (
+                TimeUtils.get_afternoon_start(timestamp) <= timestamp <= TimeUtils.get_afternoon_end(timestamp))
 
     @staticmethod
-    def next_trade_timestamp(timestamp: datetime.datetime, period=1) -> datetime.datetime:
-        year, month, day = timestamp.year, timestamp.month, timestamp.day
-        morning_start = datetime.datetime(year, month, day, 9, 30, 0)
-        morning_end = datetime.datetime(year, month, day, 11, 30, 0)
-        afternoon_start = datetime.datetime(year, month, day, 13, 0, 0)
-        afternoon_end = datetime.datetime(year, month, day, 15, 0, 0)
+    def next_trade_timestamp(timestamp: datetime, period=1) -> datetime:
         if TimeUtils.judge_trade_time(timestamp):
-            return timestamp + datetime.timedelta(seconds=period)
+            return timestamp + timedelta(seconds=period)
         else:
-            if timestamp > morning_end and timestamp < afternoon_start:
-                return afternoon_start
-            elif timestamp > afternoon_end:
-                return morning_start + datetime.timedelta(days=1)
+            if TimeUtils.get_morning_end(timestamp) < timestamp < TimeUtils.get_afternoon_end(timestamp):
+                return TimeUtils.get_afternoon_start(timestamp)
+            elif timestamp > TimeUtils.get_afternoon_end():
+                return TimeUtils.get_morning_start(timestamp) + timedelta(days=1)
             else:
-                return timestamp + datetime.timedelta(seconds=period)
+                return timestamp + timedelta(seconds=period)
